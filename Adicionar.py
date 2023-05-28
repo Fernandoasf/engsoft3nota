ae78c65c7c5e03c542ead69f50636c52f7d18d65
Fernandoasf <131702359+Fernandoasf@users.noreply.github.com>
2023-05-28 17:21:46 -0300

    modified:   Adicionar.py

--- Adicionar.py
+++ Adicionar.py
@@ -31,3 +31,37 @@ class TestAdicionarProduto(unittest.TestCase):
 if __name__ == '__main__':
     unittest.main()
 
+
+def adicionar_produto(nome, descricao, preco, quantidade):
+
+    # Convertendo o nome e a descrição para maiúsculas
+    nome = nome.upper()
+    descricao = descricao.upper()
+
+    # Conectar ao banco de dados
+    conn = psycopg2.connect(
+        database="mydatabase",
+        user="myuser",
+        password="mypassword"
+    )
+
+    # Verificar se o produto já existe
+    cursor = conn.cursor()
+    query = "SELECT COUNT(*) FROM produtos WHERE UPPER(nome) = %s AND UPPER(descricao) = %s"
+    cursor.execute(query, (nome, descricao))
+    count = cursor.fetchone()[0]
+    if count > 0:
+        cursor.close()
+        conn.close()
+        return "O produto já existe no banco de dados."
+
+    # Inserir o produto no banco de dados
+    query = "INSERT INTO produtos (nome, descricao, preco, quantidade) VALUES (%s, %s, %s, %s)"
+    cursor.execute(query, (nome, descricao, preco, quantidade))
+
+    # Confirmar a transação e fechar a conexão
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    return "Produto adicionado com sucesso."
