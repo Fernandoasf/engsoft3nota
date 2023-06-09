b65f217babc3cb449898d3fa665f92aadaed2b29
Fernandoasf <131702359+Fernandoasf@users.noreply.github.com>
2023-06-09 15:02:19 -0300

    new file:   test_adicionar.py

---
+++ test_adicionar.py
@@ -0,0 +1,15 @@
+from Estoque import Estoque
+from adicionar import adicionar_produto
+
+#teste para a função que adicuona um produto no estoque 
+def teste_adicionar_produto():
+    estoque = Estoque()
+
+    #adiciona produto cujo código ainda não está cadastrado
+    resposta = adicionar_produto(estoque, 1, 'feijão', 4.50, 30)
+    assert "Produto adicionado com sucesso" == resposta
+
+    #tenta adicionar produto cujo código já está cadastrado 
+    resposta = adicionar_produto(estoque, 1, 'arroz', 5.50, 100)
+    assert "Um produto com o mesmo código já foi adicionado" == resposta
+
