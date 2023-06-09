a702dc436a7b932e9113d0c2a14861e5474cecef
Fernandoasf <131702359+Fernandoasf@users.noreply.github.com>
2023-06-09 15:05:45 -0300

    modified:   Adicionar.py 	new file:   Estoque.py 	deleted:    Teste_adicionar.py

--- Adicionar.py
+++ Adicionar.py
@@ -1,38 +1,8 @@
-import unittest
-from unittest import mock
-import psycopg2
-import Teste_adicionar
+#função que adiciona um produto no estoque
+def adicionar_produto(estoque, codigo, nome, valor, quantidade):
 
-def adicionar_produto(nome, descricao, preco, quantidade):
-
-    # Convertendo o nome e a descrição para maiúsculas
-    nome = nome.upper()
-    descricao = descricao.upper()
-
-    # Conectar ao banco de dados
-    conn = psycopg2.connect(
-        database="mydatabase",
-        user="myuser",
-        password="mypassword"
-    )
-
-    # Verificar se o produto já existe
-    cursor = conn.cursor()
-    query = "SELECT COUNT(*) FROM produtos WHERE UPPER(nome) = %s AND UPPER(descricao) = %s"
-    cursor.execute(query, (nome, descricao))
-    count = cursor.fetchone()[0]
-    if count > 0:
-        cursor.close()
-        conn.close()
-        return "O produto já existe no banco de dados."
-
-    # Inserir o produto no banco de dados
-    query = "INSERT INTO produtos (nome, descricao, preco, quantidade) VALUES (%s, %s, %s, %s)"
-    cursor.execute(query, (nome, descricao, preco, quantidade))
-
-    # Confirmar a transação e fechar a conexão
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-    return "Produto adicionado com sucesso."
+    if codigo not in estoque.produtos:
+        estoque.produtos[codigo] = {'nome': nome, 'valor': valor, 'quantidade': quantidade}
+        return "Produto adicionado com sucesso"
+    else:
+        return "Um produto com o mesmo código já foi adicionado"
---
+++ Estoque.py
@@ -0,0 +1,4 @@
+# classe utilizada para declarar o dicionário que guardará os dados dos produtos
+class Estoque():
+    def __init__(self):
+        self.produtos = {}
